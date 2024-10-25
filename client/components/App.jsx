bc6307bc9a8f14c1aa502bdecd84cfad9fa1d1d6
Aaron Jacobs <aaronj313@gmail.com>
2024-10-25 12:13:24 -0700

    add progress on bios cards

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Info from './Info'
+import React from 'react';
+import Navbar from './Navbar';
+import Info from './Info';
 // import Install from './Install'
-// import BiosContainer from './containers/BiosContainer'
-import '../App.css'
+import BiosCard from './containers/BiosCard';
+import { Grid2 } from '@mui/material';
 
-let infoOne = "Current methods available in Kubernetes that control automatic restarts are very limited and non-customizable. We've created a tool that evaluates pod health and can automatically restart a pod based on user's custom parameters."
-let infoTwo = 'As a software engineer tasked with maintaining our Kubernetes clusters I want to be able to further customize pod restarts to recieve notifcations when a pod is having issues but more importantly NOT receive alerts and have to manually handle an issue when I could have automated it with custom restart parameters beyond the limited default Kubernetes criteria. '
+import '../App.css';
+
+let infoOne = "Current methods available in Kubernetes that control automatic restarts are very limited and non-customizable. We've created a tool that evaluates pod health and can automatically restart a pod based on user's custom parameters.";
+let infoTwo = 'As a software engineer tasked with maintaining our Kubernetes clusters I want to be able to further customize pod restarts to recieve notifcations when a pod is having issues but more importantly NOT receive alerts and have to manually handle an issue when I could have automated it with custom restart parameters beyond the limited default Kubernetes criteria. ';
 function App() {
 
   return (
     <div id='body'>
       <Navbar />
       <h1 id='tagline'>Optimize your Kubernetes Cluster</h1>
-      <div id='infoContainer'><Info text={infoOne} title='Problem/Solution'/><Info text={infoTwo} title="We're Impressive"/></div>
+      <div id='infoContainer'><Info text={infoOne} title='Problem/Solution' /><Info text={infoTwo} title="We're Impressive" /></div>
       {/* <Install /> */}
-      {/* <BiosContainer /> */}
+      <Grid2 container spacing={8}>
+        <BiosCard name="Aaron Jacobs" imgPath="../assets/teamPhotos/aaronJacobs.jpg" gitHubUrl="https://github.com/AaronJacobs" linkedInUrl="https://www.linkedin.com/in/aaronmjacobs/" emailAddress="aaronj313@gmail.com" grid2={Grid2} />
+        <BiosCard name="Alex Grimm" imgPath="../assets/fullLogo.png" gitHubUrl="" linkedInUrl="" emailAddress="" grid2={Grid2} />
+        <BiosCard name="Mason Meyer" imgPath="../assets/fullLogo.png" gitHubUrl="" linkedInUrl="" emailAddress="" grid2={Grid2} />
+        <BiosCard name="RJ McCarthy" imgPath="../assets/fullLogo.png" gitHubUrl="" linkedInUrl="" emailAddress="" grid2={Grid2} />
+        <BiosCard name="Timothy Cheng" imgPath="../assets/fullLogo.png" gitHubUrl="" linkedInUrl="" emailAddress="" grid2={Grid2} />
+      </Grid2>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/components/containers/BiosCard.jsx
+++ client/components/containers/BiosCard.jsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
-import Card from '@mui/material/Card'
+import { GitHub } from '@mui/icons-material';
+import { Avatar, Card } from '@mui/material';
 
-export default function BiosCard() {
+
+export default function BiosCard({ name, imgPath, gitHubUrl, linkedInUrl, emailAddress, Grid2 }) {
   return (
-    <Card sx={{ maxWidth: 125 }}>
-  )
+    <Card sx={{ width: 200, height: 300 }}>
+      console.log({JSON.stringify(imgPath)})
+      <Avatar sx={{ width:125, height:125 }} alt={name} src={imgPath} />
+      {/* <Grid2> */}
+      <GitHub onClick={event => window.location.href = gitHubUrl} />
+      {/* <LinkedInIcon />
+        <MailOutlineIcon /> */}
+      {/* </Grid2> */}
+    </Card>
+  );
 }
