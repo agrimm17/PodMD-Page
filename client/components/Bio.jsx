b3c18ea458701295e003d86b01fddda15c0e8c8b
agrimm17 <alexandergrimm17@gmail.com>
2024-10-25 15:11:50 -0700

    Revert "add bioCard and container with all contact info"

--- client/components/Bio.jsx
+++
@@ -1,7 +0,0 @@
-// import Card from '@mui/material/Card'
-
-// export default function BiosCard({name, imageUrl, email, linkedInUrl, gitHubUrl}) {
-//   return (
-//     <Card sx={{ maxWidth: 125 }}>
-//   )
-// }
