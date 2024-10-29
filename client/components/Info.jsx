59030a4e0840986a8db47a57663f4ebe6da64e19
agrimm17 <alexandergrimm17@gmail.com>
2024-10-29 13:58:04 -0700

    Making some font and styling updates

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 function Info({ text, title }) {
   return (
     <CardContent className='infoBox'>
-      <Typography sx={{ fontSize: 25 }}>
+      <Typography sx={{ fontSize: 25, fontFamily: "Aldrich" }}>
         <div className='infoTitle'>{title}:</div><br />{text}
       </Typography>
     </CardContent>
