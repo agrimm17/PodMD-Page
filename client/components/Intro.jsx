23f79f86af4a56e233c45493dc6a0ca69f4ecc9b
RJ McCarthy <rjmccarthy92@gmail.com>
2024-11-03 14:30:46 -0500

    New production build.

--- client/components/Intro.jsx
+++
@@ -1,8 +0,0 @@
-export default function Intro({ image, text, style }) {
-  return (
-    <div className='introCards' style={style}>
-      <div className='imageContainer'>{image}</div>
-      <div className='textContainer'>{text}</div>
-    </div>
-  );
-}
